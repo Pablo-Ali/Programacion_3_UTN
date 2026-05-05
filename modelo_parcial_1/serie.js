3328176e67a648b10e1d5ea1c9b53a5c7abc8d49
Pablo-Ali <pablosharifali@gmail.com>
2026-05-04 21:26:21 -0300

    add: modelo de parcial resuelto hasta el punto 3

---
+++ modelo_parcial_1/serie.js
@@ -0,0 +1,107 @@
+class Serie{
+
+    _id;
+    _url;
+    _name;
+    _language;
+    _generes;
+    _image;
+
+    constructor(id, url, name, language, generes, image){
+        this._id = id;
+        this._url = url;
+        this._name = name;
+        this._language = language;
+        this._generes = generes;
+        this._image = image;
+    }
+    
+    get id(){
+        return this._id;
+    }
+    set id(id){
+        this._id = id;
+    }
+
+    get url(){
+        return this._url;
+    }
+    set url(url){
+        this._url = url;
+    }
+
+    get name(){
+        return this._name;
+    }
+    set name(name){
+        this._name = name;
+    }
+
+    get language(){
+        return this._language;
+    }
+    set language(language){
+        this._language = language;
+    }
+
+    get generes(){
+        return this._generes;
+    }
+    set generes(generes){
+        this._generes = generes;
+    }
+
+    get image(){
+        return this._image;
+    }
+    set image(image){
+        this._image = image;
+    }
+
+    toJsonString(){
+        //crear un objeto limpio para pasar (para que no queden los "_" antes de cada atributo)
+        const obj = {
+            id: this._id,
+            url: this._url,
+            name: this._name,
+            language: this._language,
+            generes: this._generes,
+            image: this._image
+        };
+        //creo el JSON a partir del objeto y lo retorno
+        return JSON.stringify(obj);
+    }
+
+    static createFromJsonString(json){
+        //parsear
+        const obj = JSON.parse(json);
+
+        //creo la nueva serie con los datos del JSON y la retorno
+        return new Serie(obj.id, obj.url, obj.name, obj.language, obj.generes, obj.image);
+    }
+
+    createHtmlElement(){
+        //creo el contenedor
+        const div = document.createElement("div");
+
+        //creo elementos
+        const h2Nombre = document.createElement("h2");
+        const pLanguage = document.createElement("p");
+        const pGenres = document.createElement("p");
+        const imgImagen = document.createElement("img");
+
+        let cadena = "";
+        for (let g of this._generes){
+            cadena += g + " ";
+        }
+        //asigno los elementos
+        h2Nombre.textContent = this._name;
+        pLanguage.textContent = this._language;
+        pGenres.textContent = cadena;
+        imgImagen.src = this._image;
+
+        //asigno los elementos al contenedor y retorno
+        div.append(h2Nombre, pLanguage,pGenres,imgImagen);
+        return div;
+    }
+}
